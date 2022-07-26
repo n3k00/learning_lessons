6d3f749e1912edc32985751f9a53cea51b5d64e3
n3k00 <shinthantaung@ucspin.edu.mm>
2022-07-26 10:49:24 +0630

    column,risedbutton,anonymous function

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  void answerQuestion(){
+    print("Answer Chosen!");
+  }
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my default text!"),
+        body: Column(
+          children: [
+            Text("The question!"),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 1 "),
+            ),
+            RaisedButton(
+              onPressed: ()=>print("Answer 2 chosen!"),
+              child: Text("Answer 2 "),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print("Answer 3 chosen!");
+              },
+              child: Text("Answer 3 "),
+            ),
+          ],
+        ),
       ),
     );
   }
