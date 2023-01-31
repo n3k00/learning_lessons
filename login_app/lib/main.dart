7fbb9f5fcc88bcbd4aea7aa0d409f090075a934b
n3k00 <shinthantaung@ucspin.edu.mm>
2023-01-31 21:54:55 +0630

    Login Simple App

---
+++ login_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Testing one")),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
