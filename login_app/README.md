f51cc6152adc748f707b49620349f4d9a05b5abf
n3k00 <shinthantaung@ucspin.edu.mm>
2023-02-04 15:08:10 +0630

    Simple Login Screen

--- login_app/README.md
+++ login_app/README.md
@@ -1,16 +1,10 @@
 # login_app
 
-A new Flutter project.
+Simple Login Screen 
+ 
+I am learning flutter widgets .
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+![screenshot image](screenshot.png "Title")
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,19 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Testing one")),
-        body: Center(
-          child: Text("Hello World"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 0, 0, 1.0),
+        title: Text(
+          "Login",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        leading: Icon(
+          Icons.chevron_left,
+          size: 32,
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        color: Color.fromRGBO(16, 16, 16, 1.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(38, 38, 38, 1.0),
+                  hintText: "Email",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(38, 38, 38, 1.0),
+                  hintText: "Password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: null,
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: null,
+                child: Text(
+                  "Recover Password",
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
